c5f89fa3599b16e2aad30d457b65c152d28608f2
Metaliy <darkspirtx@gmail.com>
2024-06-11 15:36:17 +0300

    fsd-folders

--- front/.eslintrc.cjs
+++ front/.eslintrc.cjs
@@ -1,18 +1,37 @@
 module.exports = {
   root: true,
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2020: true,
+  },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
     'plugin:react-hooks/recommended',
+    'plugin:jsx-a11y/recommended', // Добавлено для поддержки доступности
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+  plugins: ['react-refresh', '@typescript-eslint', 'react', 'jsx-a11y'],
   rules: {
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    'react/react-in-jsx-scope': 'off', // Отключение правила импорта React
+    // Дополнительные правила ESLint
   },
-}
+};
